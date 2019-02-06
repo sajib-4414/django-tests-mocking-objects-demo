49790c558ef6f8172197930f5e81bddebd643722
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2019-02-06 13:40:03 +0600

    implemented fixtures in some tests from the initial video series

--- products/tests/test_models.py
+++ products/tests/test_models.py
@@ -1,6 +1,9 @@
 from mixer.backend.django import mixer
 import pytest
 
+from products.models import Product
+
+
 @pytest.mark.django_db
 class TestModels:
 
@@ -11,3 +14,17 @@ class TestModels:
     def test_product_is_not_in_stock(self):
         product = mixer.blend('products.Product', quantity=0)
         assert product.is_in_stock == False
+
+#--------------NOW with fixtures
+@pytest.fixture
+def product(request,db):
+    return mixer.blend(Product,quantity=request.param)
+
+
+@pytest.mark.parametrize('product',[1], indirect=True)
+def test_product_is_in_stock(product):
+    assert product.is_in_stock == True
+
+@pytest.mark.parametrize('product',[0], indirect=True)
+def test_product_is_not_in_stock(product):
+    assert product.is_in_stock == False
--- products/tests/test_views.py
+++ products/tests/test_views.py
@@ -79,3 +79,40 @@ class TestViews(TestCase):
         response = product_detail(request,pk=1)
         #checking if the response contains this hit to login
         assert "accounts/login" in response.url
+
+#--------------------------Now doing the same test using Fixtures
+
+@pytest.fixture(scope='module')
+def factory():
+    print("Factory instantiated")
+    return RequestFactory()
+
+@pytest.fixture
+def product(db):
+    return mixer.blend(Product)
+
+def test_product_detail_authenticated(factory, product):
+    #taking the path and by supplying kwargs creating a full path
+    path = reverse('detail',kwargs={'pk':1})
+    #mocking an HTTP request
+    request = factory.get(path)
+    #creatng a mock user and assigns it to the request
+    request.user = mixer.blend(User)
+
+    #mocking, passing an HTTP request with parameters in the view method
+    response = product_detail(request,pk=1)
+    #checking if it gives status 200 or not, we are passing an user, so it is authenticated call. So it will return 200
+    assert response.status_code == 200
+
+def test_product_detail_unauthenticated(factory,product):
+    #taking the path and by supplying kwargs creating a full path
+    path = reverse('detail',kwargs={'pk':1})
+    #mocking an HTTP request
+    request = factory.get(path)
+    #creatng a non logged in user scenario
+    request.user = AnonymousUser()
+
+    #mocking, passing an HTTP request with parameters in the view method
+    response = product_detail(request,pk=1)
+    #checking if the response contains this hit to login
+    assert "accounts/login" in response.url
